cd786f2b2b9d58459bc7492394269d2b5df1d278
dhimasarista <dhimasharrisst@gmail.com>
2024-03-26 13:29:49 +0700

    Change: Setup Route

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const expressLayouts = require("express-ejs-layouts");
 const http = require("http");
 const path = require("path");
 const SetupRoutes = require("./app/routes/routes");
+const EmployeeRoute = require("./app/routes/employee_routes");
+const ErrorRoutes = require("./app/routes/error_routes");
+const DepartmentRoutes = require("./app/routes/department_routes");
+const UserManagementRoutes = require("./app/routes/user_management_routes");
 
 const app = express(); // Init aplikasi
 const port = 3000; // Init port
@@ -17,9 +21,13 @@ app.set("view engine", "ejs"); // Mengatur View Engine ke EJS
 app.set("views", path.join(__dirname, "views")); // Mengatur path ke folder views
 app.use(express.static(path.join(__dirname, "wwwroot"))); // Mengatur akses layanan file static dari folder wwwroot
 app.use(express.json()); // Parsing permintaan JSON
-SetupRoutes(app); // Mengatur semua route yang bisa diakses
+// Routes
+new EmployeeRoute(app);     
+new ErrorRoutes(app);
+new DepartmentRoutes(app);
+new UserManagementRoutes(app);
+// Middleware untuk menangani routing yang tidak ada
 app.use((req, res) => {
-    // Middleware untuk menangani routing yang tidak ada
     return res.status(404).redirect("/404");
 })
 
