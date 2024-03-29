e29a29621b492d145d364e405cc9825440ec683e
dhimasarista <dhimasharrisst@gmail.com>
2024-03-29 10:51:29 +0700

    Change: Handlers name

--- app/routes/department_routes.js
+++ app/routes/department_routes.js
@@ -1,5 +1,5 @@
-const { render } = require("../controllers/department_controllers");
-const { findAll, insert } = require("../controllers/position_controllers");
+const { renderDepartment, newDepartment } = require("../controllers/department_controllers");
+const { findAllPositions, newPosition} = require("../controllers/position_controllers");
 class DepartmentRoutes{
     constructor(app){
         this.app = app;
@@ -8,12 +8,13 @@ class DepartmentRoutes{
     }
 
     department(){
-        this.app.get("/departments", render);
+        this.app.get("/departments", renderDepartment);
+        this.app.post("/department", newDepartment);
     }
 
     position(){
-        this.app.get("/positions", findAll);
-        this.app.post("/position/new", insert);
+        this.app.get("/positions", findAllPositions);
+        this.app.post("/position/new", newPosition);
     }
 }
 
