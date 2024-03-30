824eda49a4f016d6db498fe3bcf768da0e7d34a1
dhimasarista <dhimasharrisst@gmail.com>
2024-03-30 16:50:42 +0700

    Change: Model method

--- app/controllers/department_controllers.js
+++ app/controllers/department_controllers.js
@@ -2,16 +2,19 @@
 const UserModel = require("../models/user_model");
 const DepartmentModel = require("../models/department_model");
 const PositionModel = require("../models/position_model");
+const DepartmentPositionModel = require("../models/department_position_model");
+
 const user = new UserModel();
 const department = new DepartmentModel();
 const position = new PositionModel();
+const departmentPosition = new DepartmentPositionModel();
 module.exports = {
     renderDepartment: async (req, res) => {
         const users = user.GetUserByID(2004);
         const username = users["username"];
         const path = req.path;
 
-        const findAllDepartment = await department.findDepartmentWithPosition();
+        const findAllDepartment = await departmentPosition.findAll();
         const findAllPosition = await position.findAll();
         return res.render("departments_page", {
             username: username,
