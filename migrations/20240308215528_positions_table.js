ee8ab6d21cfbf059b35cf0cd6eb0e42994ba46af
Dhimas Arista <dhimasharrisst@gmail.com>
2024-04-02 07:45:43 +0100

    Delete: department foreign key

--- migrations/20240308215528_positions_table.js
+++ migrations/20240308215528_positions_table.js
@@ -6,8 +6,8 @@ exports.up = function(knex) {
     return knex.schema.createTable('positions', function(table) {
         table.increments('id').primary(); // Kolom ID sebagai primary key
         table.string('name').unique(); // Kolom Username sebagai string unik
-        table.integer('department_id').unsigned(); // Kolom department_id sebagai integer unsigned
-        table.foreign('department_id').references('id').inTable('departments'); // Kunci asing ke tabel employees
+        // table.integer('department_id').unsigned(); // Kolom department_id sebagai integer unsigned
+        // table.foreign('department_id').references('id').inTable('departments'); // Kunci asing ke tabel employees
         table.timestamp('created_at').defaultTo(knex.fn.now()); // Kolom created_at dengan nilai default saat ini
         table.timestamp('updated_at').defaultTo(knex.fn.now()); // Kolom updated_at dengan nilai default saat ini
         table.timestamp('deleted_at').nullable(); // Kolom deleted_at dengan nilai nullabel
