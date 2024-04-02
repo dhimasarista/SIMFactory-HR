80a2289a5a02fd3d2d4e04a0e42c4cd3ef51b7ff
dhimasarista <dhimasharrisst@gmail.com>
2024-04-03 03:26:32 +0700

    Fixing: employee department & positions form selection

--- app/controllers/employee_controllers.js
+++ app/controllers/employee_controllers.js
@@ -1,7 +1,9 @@
 const UserModel = require("../models/user_model");
 const EmployeeModel = require("../models/employee_model");
+const DepartmentModel = require("../models/department_model");
 const userModel = new UserModel();
 const employeeModel = new EmployeeModel();
+const departmentModel = new DepartmentModel();
 module.exports = {
     render: async (req, res) => {
         const user = userModel.GetUserByID(2004);
@@ -9,10 +11,13 @@ module.exports = {
         const path = req.path;
 
         const employees = await employeeModel.findAll();
+        const departments = await departmentModel.findAll();
+        console.log(departments);
         return res.render("employees_page", {
             username: username,
             path: path,
             employees: employees,
+            departments: departments
         });
     },
     deleteEmployee: async (req, res) => {
