4889a59bc5162c35990da356f8d875aa18198806
dhimasarista <dhimasharrisst@gmail.com>
2024-04-25 00:38:27 +0700

    Attach: date formatting

--- app/utilities/formatting_date.js
+++ app/utilities/formatting_date.js
@@ -1,4 +1,6 @@
-module.exports = (dateString) => {
+function formatDate (dateString) {
     const [month, day, year] = dateString.split('-');
     return `${year}-${month}-${day}`;
 }
+
+module.exports = formatDate;
