dd4f0e41e344f31ee38e3e72750750c251011108
dhimasarista <dhimasharrisst@gmail.com>
2024-04-26 00:30:42 +0700

    Fixing: get image

--- app/routes/file_routes.js
+++ app/routes/file_routes.js
@@ -1,20 +1,40 @@
 const path = require('path');
+const fs = require("fs");
 
 class FileRoutes{
     constructor(app){
-        const idCardsDirectory = path.join(__dirname, 'app', 'uploads', 'idcards');
-        const photosDirectory = path.join(__dirname, 'app', 'uploads', 'photos');
-        app.get("/image/idcards/:name", (req, res) => {
+        const idCardsDirectory = path.join(__dirname, '../', 'uploads', 'idcards');
+        const photosDirectory = path.join(__dirname, '../', 'uploads', 'photos');
+        app.get("/image/idcard/:name", (req, res) => {
             const name = req.params.name;
+            console.log(name);
             const filePath = path.join(idCardsDirectory, name);
 
-            return res.send(filePath);
+            fs.readFileSync(filePath, (err, data) => {
+                if (err) {
+                    return res.status(500).json({
+                        error: err,
+                    });
+                }
+
+                res.setHeader('Content-Type', 'application/octet-stream');
+                res.send(data);
+            });
         });
         app.get("/image/photo/:name", (req, res) => {
             const name = req.params.name;
             const filePath = path.join(photosDirectory, name);
 
-            return res.send(filePath);
+            fs.readFile(filePath, (err, data) => {
+                if (err) {
+                    return res.status(500).json({
+                        error: err,
+                    });
+                }
+
+                res.setHeader('Content-Type', 'application/octet-stream');
+                res.send(data);
+            });
         });
     }
 }
