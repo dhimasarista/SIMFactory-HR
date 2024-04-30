a6b65da2fa418cdfdc1f879a7a95c5894de8257f
dhimasarista <dhimasharrisst@gmail.com>
2024-05-01 01:37:58 +0700

    Fixing: type of image form

--- app/routes/upload_routes.js
+++ app/routes/upload_routes.js
@@ -1,6 +1,9 @@
 const {upload, deleteImage} = require("../controllers/upload_controllers");
 const compressAndSaveImage = require("../utilities/compress_image");
-
+const path = require("path");
+const fs = require("fs");
+const idCardsDirectory = path.join(__dirname, '../', 'uploads', 'idcards');
+const photosDirectory = path.join(__dirname, '../', 'uploads', 'photos'); 
 class UploadRoutes{
     constructor(app){
         this.app = app;
@@ -27,13 +30,24 @@ class UploadRoutes{
             }
         });
         // Routing Menghapus File
-        this.app.get("/delete/image/:filename", (req, res) => {
+        this.app.post("/delete/image/:filename", (req, res) => {
             const filename = req.params.filename;
+            const { type_file, type_form } = req.body;
             try {
-                deleteImage(filename, "../uploads/");
+                if (type_form === "new-form") {
+                    deleteImage(filename, "../uploads/");   
+                } else if (type_form === "edit-form") {
+                    // Memeriksa di sub direktori
+                    if (type_file === "idcard") {
+                        deleteImage(filename, "../uploads/idcards/");
+                    } else if (type_file === "photo") {
+                        deleteImage(filename, "../uploads/photos/");
+                    } 
+                }
                 return res.json({
                     status: 200,
-                    message: `Image ${filename} has been deleted from server.`
+                    message: `Image ${filename} has been deleted from server.`,
+                    body: req.body,
                 });
             } catch (error) {
                 return res.json({
